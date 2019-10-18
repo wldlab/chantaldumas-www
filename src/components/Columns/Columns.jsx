4b79a4104c8204ebfbe570bcf7e05254164a095d
Marc-Antoine Ruel <hello@marcantoineruel.com>
2019-10-18 17:17:04 -0400

    create columns component

---
+++ src/components/Columns/Columns.jsx
@@ -0,0 +1,43 @@
+// vendors
+import React from "react"
+import { css } from "@emotion/core"
+import mediaQuery from "../../utils/media-query"
+import PropTypes from "prop-types"
+import styled from "@emotion/styled"
+
+const Columns = ({ columns, children, ...rest }) => (
+  <div
+    css={css`
+      columns: 1;
+      column-gap: 30px;
+
+      ${mediaQuery.greaterThen(768)} {
+        columns: ${columns};
+      }
+    `}
+    {...rest}
+  >
+    {children}
+  </div>
+)
+
+Columns.propTypes = {
+  children: PropTypes.node.isRequired,
+  columns: PropTypes.number,
+}
+
+Columns.defaultProps = {
+  columns: 2,
+}
+
+export const preventColumnBreakStyles = css`
+  break-inside: avoid; /* Chrome, Safari */
+  page-break-inside: avoid; /* Theoretically FF 20+ */
+  display: table; /* Actually FF 20+ */
+`
+
+export const UnbreakableBlock = styled.div`
+  ${preventColumnBreakStyles};
+`
+
+export default Columns
---
+++ src/components/Columns/index.js
@@ -0,0 +1,5 @@
+import Columns, { preventColumnBreakStyles, UnbreakableBlock } from "./Columns"
+
+export default Columns
+
+export { preventColumnBreakStyles, UnbreakableBlock }
