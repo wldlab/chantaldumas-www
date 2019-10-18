f59cad51eb21331686361533ca4c60e40ff9f382
Marc-Antoine Ruel <hello@marcantoineruel.com>
2019-10-18 15:50:27 -0400

    install react-intl package

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,5 +3,28 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
+import React from "react"
+import { IntlProvider, addLocaleData } from "react-intl"
 
-// You can delete this file if you're not using it
+// locales data
+// import frLocaleData from "react-intl/locale-data/fr"
+// import enLocaleData from "react-intl/locale-data/en"
+
+import fr from "./src/locales/fr.json"
+import en from "./src/locales/en.json"
+
+// addLocaleData([...frLocaleData, ...enLocaleData])
+
+const messages = { fr, en }
+
+export const wrapPageElement = ({ element, props }) => {
+  const {
+    pageContext: { langKey },
+  } = props
+
+  return (
+    <IntlProvider locale={langKey} messages={messages[langKey]}>
+      {element}
+    </IntlProvider>
+  )
+}
