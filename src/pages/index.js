20c8ed2373ce8627853d8542ce508fe97cb02326
Marc-Antoine Ruel <hello@marcantoineruel.com>
2019-10-18 12:09:04 -0400

    add testing AlbumCard in index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,46 @@ import React from "react"
 
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
+import AlbumCard from "../components/AlbumCard/AlbumCard"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-  </Layout>
-)
+const IndexPage = () => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+
+      <AlbumCard title="Le petit homme dans l’oreille">
+        <p>
+          Le petit homme dans l’oreille (2000)
+          <br />
+          Œuvre sonore de Christian Calon et Chantal Dumas
+          <br />
+          Durée : 57 min
+        </p>
+        <p>
+          Été.
+          <br />9 juillet–9 septembre 1999.
+          <br />
+          20 000 km sur les routes et les pistes du Canada. Montée de Montréal
+          au cercle arctique (Yukon) à travers les Prairies, descente vers le
+          Pacifique et retour par les Badlands. Remplie d’équipements de prise
+          de son, d’outils, de cassettes DAT, tente, Coleman, sacs de couchage,
+          ustensiles de cuisine, pneu de secours, bières, appareil photo,
+          bottes, livres et cartes routières, la minivan Mercury prit la route.
+        </p>
+        <p>
+          Commandée par Mario Gauthier pour l’émission L’espace du son, cette
+          œuvre a été produite par la Chaîne culturelle de Radio-Canada
+          (Montréal, Canada). L’accompagnement de Rob Dramer, de Lillian Ireland
+          et de Mike Krutko a également permis la réalisation de ce parcours
+          sonore, tout comme les maintes voix anonymes qui l’ont inspiré.
+        </p>
+        <p>
+          Le petit homme dans l’oreille a été enregistrée sur CD et publiée dans
+          le coffret de deux disques radio roadmovies.
+        </p>
+      </AlbumCard>
+    </Layout>
+  )
+}
 
 export default IndexPage
