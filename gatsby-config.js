6f36a27fbade0aa56ab15f5b66f15c160677debc
Marc-Antoine Ruel <hello@marcantoineruel.com>
2019-10-18 14:13:26 -0400

    add data and artists markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,14 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data`,
+      },
+    },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
