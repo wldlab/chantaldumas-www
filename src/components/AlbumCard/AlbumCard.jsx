865b7e4bfe18da09338ecc52f02bc1a3cff642d5
Marc-Antoine Ruel <hello@marcantoineruel.com>
2019-10-18 12:08:41 -0400

    create AlbumCard component

---
+++ src/components/AlbumCard/AlbumCard.jsx
@@ -0,0 +1,54 @@
+// vendors
+import React from "react"
+import { css } from "@emotion/core"
+import PlayButton from "../PlayButton/PlayButton"
+import { typography } from "../../styles/styles"
+import { forcingBreakingWord } from "../../utils/forcing-breaking-word"
+
+const AlbumCard = ({ title, children, tracks }) => {
+  return (
+    <div
+      css={css`
+        display: grid;
+        grid-gap: 30px;
+        grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+        max-width: 1348px;
+        margin: auto;
+      `}
+    >
+      <div
+        css={css`
+          display: flex;
+          flex-flow: column;
+        `}
+      >
+        <p
+          css={css`
+            font-family: ${typography.type.display};
+            font-size: ${120 / 25}em;
+            line-height: ${140 / 120};
+            font-weight: ${typography.weight.medium};
+            margin: 0;
+            ${forcingBreakingWord}
+          `}
+        >
+          {title}
+        </p>
+
+        <PlayButton
+          css={css`
+            font-size: ${120 / 25}em;
+          `}
+        />
+      </div>
+
+      <div>{children}</div>
+    </div>
+  )
+}
+
+AlbumCard.defaultProps = {
+  tracks: [],
+}
+
+export default AlbumCard
