1a00816229aee1e1397c9bee0f9f5ebc5e06f599
Marc-Antoine Ruel <hello@marcantoineruel.com>
2019-10-29 16:02:08 -0400

    add Player and PlayerContext to Layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -7,27 +7,33 @@ import "../../Fonts/typographies.css"
 import { globalStyle } from "../../styles/global"
 import mediaQuery from "../../utils/media-query"
 import { between } from "polished"
+import Player from "../Player"
+import { PlayerProvider } from "../../context/PlayerContext"
 
 const Layout = ({ children }) => {
   return (
     <>
-      <Global styles={globalStyle} />
-      <div
-        css={css`
-          max-width: 1624px;
-          margin: auto 36px;
+      <PlayerProvider>
+        <Global styles={globalStyle} />
+        <div
+          css={css`
+            max-width: 1624px;
+            margin: auto 36px;
 
-          ${mediaQuery.greaterThen(1024)} {
-            margin: auto ${between("36px", "147px", "1024px", "1920px")};
-          }
+            ${mediaQuery.greaterThen(1024)} {
+              margin: auto ${between("36px", "147px", "1024px", "1920px")};
+            }
 
-          ${mediaQuery.greaterThen(1920)} {
-            margin: auto;
-          }
-        `}
-      >
-        <main>{children}</main>
-      </div>
+            ${mediaQuery.greaterThen(1920)} {
+              margin: auto;
+            }
+          `}
+        >
+          <main>{children}</main>
+        </div>
+
+        <Player />
+      </PlayerProvider>
     </>
   )
 }
